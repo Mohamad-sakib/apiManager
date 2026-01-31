99209413b9d8a9f6418dbec42b0ae82a89cd842a
Mohamad-sakib <ms4184sakib@gmail.com>
2026-01-31 18:50:29 +0530

    add button to remove post

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,9 +1,11 @@
-export function Post({ post }) {
-  const { title, body } = post;
+export function Post({ post, handleDelete }) {
+  const { id, title, body } = post;
+
   return (
     <div>
       <h1 className="title">{title}</h1>
       <div className="body">{body}</div>
+      <button onClick={() => handleDelete(id)}>delete</button>
     </div>
   );
 }
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Post } from "./Post";
-import { getPosts } from "../services/PostApis";
+import { getPosts, removePost } from "../services/PostApis";
 import { PostForm } from "./PostForm";
 
 export function Posts() {
@@ -12,11 +12,19 @@ export function Posts() {
       .catch((error) => console.error(error));
   }, []);
 
+  const handleDelete = (id) => {
+    removePost(id)
+      .then(() => {
+        setPosts(posts.filter((post) => post.id !== id));
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div>
       <PostForm posts={posts} setPosts={setPosts} />
       {posts.map((post) => (
-        <Post key={post.id} post={post} />
+        <Post key={post.id} post={post} handleDelete={handleDelete} />
       ))}
     </div>
   );
